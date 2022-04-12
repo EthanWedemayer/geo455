cf14d3d662ff78d8206c0667a7100b3c0e595940
EthanWedemayer <98429395+EthanWedemayer@users.noreply.github.com>
2022-04-12 16:47:12 -0500

    Add files via upload

---
+++ lab_9/lab9_copy.js
@@ -0,0 +1,36 @@
+ var lrmap = L.map('map').setView([42.90582784366248, -89.35372165627099], 8);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw')
+        .addTo(lrmap);
+var grayscale = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+   attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
+   maxZoom: 11,
+   minZoom: 2
+ }).addTo(lrmap);
+
+
+var migrationLayer = new L.migrationLayer({
+    map: lrmap,
+    data: data,
+    pulseRadius:30,
+    pulseBorderWidth:3,
+    arcWidth:1,
+    arcLabel:true,
+    arcLabelFont:'10px sans-serif',
+    maxWidth:10
+    }
+);
+migrationLayer.addTo(lrmap);
+
+
+function hide(){
+    migrationLayer.hide();
+}
+function show(){
+    migrationLayer.show();
+}
+function play(){
+    migrationLayer.play();
+}
+function pause(){
+    migrationLayer.pause();
+}
